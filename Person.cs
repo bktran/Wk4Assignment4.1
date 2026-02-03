54b2e3cca90627a101469a5451a6fe04b8600048
Bao Khanh Tran <ktbv.22@outlook.com>
2026-02-02 16:55:52 -0800

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wk4Assignment4._1
+{
+    internal class Person
+    {
+        public int FirstName { get; set; }
+        public int LastName { get; set; }
+        public long Mobile  { get; set; }
+        public long Work { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ PhoneBook.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wk4Assignment4._1
+{
+    internal class PhoneBook
+    {
+        public static Dictionary<string, Person> Contacts = new Dictionary<string, Person>();
+    }
+}
